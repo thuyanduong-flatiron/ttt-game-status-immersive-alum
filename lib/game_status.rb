a6056ff5c4109d6f8b471700ba827ad52663a559
Thuyan Duong <thuyan.duong@flatironschool.com>
2018-10-21 22:44:17 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -21,9 +21,9 @@ def won?(board)
     win_index_2 = win_combination[1]
     win_index_3 = win_combination[2]
    
-    position_1 = board[win_index_1] # load the value of the board at win_index_1
-    position_2 = board[win_index_2] # load the value of the board at win_index_2
-    position_3 = board[win_index_3] # 
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2] 
+    position_3 = board[win_index_3] 
     if board[win_combination[0]] == board[win_combination[1]] && board[win_combination[1]] == board[win_combination[2]]
   end
 end
