5aa193dd0cde6fbea600d7dae3ac1453193938cc
Thuyan Duong <thuyan.duong@flatironschool.com>
2018-10-21 22:43:21 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,6 +17,6 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   WIN_COMBINATIONS.each do |win_combination|
-    if board[win_combination[0]] == board[win_combination
+    if board[win_combination[0]] == board[win_combination[1]] && board[win_combination[1]] == board[win_combination[2]]
   end
 end
